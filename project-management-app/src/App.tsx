81905275c058f9ab33493da51e8850cc3d3c1b69
KononOleg <konon.oleg97@gmail.com>
2022-11-18 15:15:21 +0300

    feat: implement router

--- project-management-app/src/App.tsx
+++ project-management-app/src/App.tsx
@@ -3,12 +3,14 @@ import './helpers/i18n';
 import { FC, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/index';
 
 const App: FC = () => {
   return (
     <Suspense fallback={<div>...</div>}>
       <Provider store={store}>
-        <></>
+        <RouterProvider router={router} />
       </Provider>
     </Suspense>
   );
