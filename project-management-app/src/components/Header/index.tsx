9a02b3409af8a0f2a79f8b7c7b3d8aec49034a6e
KononOleg <konon.oleg97@gmail.com>
2022-11-20 15:17:57 +0300

    fix: header logo

--- project-management-app/src/components/Header/index.tsx
+++ project-management-app/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import './styles.css';
 import { FC } from 'react';
-import logo from '../../assets/icon/trello.png';
+import logo from '../../assets/icon/trello-logo.svg';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from '../LanguageSwitcher';
 
@@ -9,10 +9,7 @@ const Header: FC = () => {
 
   return (
     <header className="header__wrapper">
-      <a className="header__logo" href="/">
-        <img className="logo__img" src={logo} />
-        <h1 className="logo__title">Trello</h1>
-      </a>
+      <img className="header__logo " src={logo} />
       <div className="header__buttons">
         <LanguageSwitcher />
         <a className="header__signin" href="signin">
