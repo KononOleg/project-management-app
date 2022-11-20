3bb4d863869230c2c293023482beabf0d748fcf2
KononOleg <konon.oleg97@gmail.com>
2022-11-20 14:57:15 +0300

    feat: add authorization pages

---
+++ project-management-app/src/pages/Authorization/index.tsx
@@ -0,0 +1,90 @@
+import './styles.css';
+import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import logo from '../../assets/icon/trello-logo.svg';
+
+interface IFormInputs {
+  name: string;
+  login: string;
+  password: string;
+}
+
+interface IProps {
+  isSignUp?: boolean;
+}
+
+const Authorization: FC<IProps> = ({ isSignUp }) => {
+  const {
+    register,
+    formState: { errors, isDirty },
+    handleSubmit,
+  } = useForm<IFormInputs>();
+  const { t } = useTranslation();
+
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => alert(JSON.stringify(data));
+
+  return (
+    <div className="authorization__wrapper">
+      <img src={logo} className="authorization__logo" />
+      <form onSubmit={handleSubmit(onSubmit)} className="authorization__form">
+        <h3 className="authorization-form__title">
+          {isSignUp ? t('AUTHORIZATION.TITLE_SIGNUP') : t('AUTHORIZATION.TITLE_SIGNIN')}
+        </h3>
+
+        {isSignUp && (
+          <div className="authorization-input__wrapper">
+            <input
+              placeholder={t('AUTHORIZATION.NAME_PLACEHOLDER') as string}
+              {...register('name', { required: t('AUTHORIZATION.NAME_ERROR') as string })}
+              className={`authorization__input ${errors.name && 'authorization__input_error'}`}
+            />
+            {errors.name && <p className="authorization-input__error">{errors?.name.message}</p>}
+          </div>
+        )}
+
+        <div className="authorization-input__wrapper">
+          <input
+            placeholder={t('AUTHORIZATION.LOGIN_PLACEHOLDER') as string}
+            {...register('login', { required: t('AUTHORIZATION.LOGIN_ERROR') as string })}
+            className={`authorization__input ${errors.login && 'authorization__input_error'}`}
+          />
+          {errors.login && <p className="authorization-input__error">{errors?.login.message}</p>}
+        </div>
+        <div className="authorization-input__wrapper">
+          <input
+            placeholder={t('AUTHORIZATION.PASSWORD_PLACEHOLDER') as string}
+            type="password"
+            {...register('password', {
+              required: t('AUTHORIZATION.PASSWORD_ERROR') as string,
+              minLength: { value: 8, message: t('AUTHORIZATION.PASSWORD_ERROR_LENGTH') },
+            })}
+            className={`authorization__input ${errors.password && 'authorization__input_error'}`}
+          />
+          {errors.password && (
+            <p className="authorization-input__error">{errors?.password.message}</p>
+          )}
+        </div>
+        <input
+          type="submit"
+          className="authorization__submit"
+          disabled={!isDirty}
+          value={t('AUTHORIZATION.SUBMIT') as string}
+        />
+
+        {isSignUp ? (
+          <Link to="/signin" className="authorization__link">
+            {t('AUTHORIZATION.LINK_SINGIN')}
+          </Link>
+        ) : (
+          <Link to="/signup" className="authorization__link">
+            {t('AUTHORIZATION.LINK_SINGUP')}
+          </Link>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default Authorization;
--- project-management-app/src/routes/index.tsx
+++ project-management-app/src/routes/index.tsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Authorization from '../pages/Authorization';
 import WelcomePage from '../pages/WelcomePage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <WelcomePage />,
+  },
+  {
+    path: '/signup',
+    element: <Authorization isSignUp={true} />,
+  },
+  {
+    path: '/signin',
+    element: <Authorization />,
   },
 ]);
 
