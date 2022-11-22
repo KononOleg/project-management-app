202ebd7e972e091c159a632098570cdfc17f64bf
KononOleg <konon.oleg97@gmail.com>
2022-11-22 11:05:26 +0300

    feat: add header authorization logic

--- project-management-app/src/components/Header/index.tsx
+++ project-management-app/src/components/Header/index.tsx
@@ -4,9 +4,11 @@ import logo from '../../assets/icon/trello-logo.svg';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from '../LanguageSwitcher';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../hooks/redux';
 
 const Header: FC = () => {
   const { t } = useTranslation();
+  const { isAuth } = useAppSelector((state) => state.AuthReducer);
 
   return (
     <header className="header__wrapper">
@@ -15,12 +17,16 @@ const Header: FC = () => {
       </Link>
       <div className="header__buttons">
         <LanguageSwitcher />
-        <Link to="signin" className="header__signin">
-          {t('LOGIN_LINK')}
-        </Link>
-        <Link to="signup" className="header__signup">
-          {t('SIGNUP_LINK')}
-        </Link>
+        {!isAuth && (
+          <>
+            <Link to="signin" className="header__signin">
+              {t('LOGIN_LINK')}
+            </Link>
+            <Link to="signup" className="header__signup">
+              {t('SIGNUP_LINK')}
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
