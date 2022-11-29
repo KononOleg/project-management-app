c12dda133267cde6498c71233f3b2acd8154b1ab
KononOleg <konon.oleg97@gmail.com>
2022-11-29 14:32:28 +0300

    refactor: auth silce

--- project-management-app/src/store/reducers/AuthSlice.ts
+++ project-management-app/src/store/reducers/AuthSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IUser } from '../../types';
-import { AxiosErrorDataType } from '../../types/api';
 import { checkIsAuth, signIn, signUp } from '../thunks/AuthThunks';
 
 interface AuthState {
