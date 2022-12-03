66df12e485b5fdc31226b761bcd4cb244c7262b0
KononOleg <konon.oleg97@gmail.com>
2022-12-03 18:00:38 +0300

    refactor: add tasks service

--- project-management-app/src/service/BoardService.ts
+++ project-management-app/src/service/BoardService.ts
@@ -1,6 +1,6 @@
 import $api from './http';
 import { AxiosResponse } from 'axios';
-import { IColumn, ITask } from '../types';
+import { IColumn } from '../types';
 
 export default class BoardService {
   static async getColumns(boardId: string): Promise<AxiosResponse<IColumn[]>> {
@@ -32,79 +32,5 @@ export default class BoardService {
 
   static async deleteColumn(boardId: string, columnId: string): Promise<AxiosResponse> {
     return await $api.delete(`/boards/${boardId}/columns/${columnId}`);
-  }
-
-  static async getTasks(boardId: string, columnId: string): Promise<AxiosResponse<ITask[]>> {
-    return await $api.get<ITask[]>(`/boards/${boardId}/columns/${columnId}/tasks`);
-  }
-
-  static async addTask(
-    boardId: string,
-    columnId: string,
-    title: string,
-    order: number,
-    description: string,
-    userId: string
-  ): Promise<AxiosResponse<ITask>> {
-    return await $api.post<ITask>(`/boards/${boardId}/columns/${columnId}/tasks`, {
-      title,
-      order,
-      description,
-      userId,
-      boardId,
-      columnId,
-      users: [],
-    });
-  }
-
-  static async updateTask(
-    boardId: string,
-    columnId: string,
-    taskId: string,
-    title: string,
-    order: number,
-    description: string,
-    userId: string,
-    users: string[]
-  ): Promise<AxiosResponse<ITask>> {
-    return await $api.put<ITask>(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`, {
-      title,
-      order,
-      description,
-      userId,
-      boardId,
-      columnId,
-      users,
-    });
-  }
-
-  static async transferTask(
-    boardId: string,
-    columnId: string,
-    toColumnId: string,
-    taskId: string,
-    title: string,
-    order: number,
-    description: string,
-    userId: string,
-    users: string[]
-  ): Promise<AxiosResponse | undefined> {
-    return await $api.put(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`, {
-      title,
-      order,
-      description,
-      userId,
-      boardId,
-      columnId: toColumnId,
-      users,
-    });
-  }
-
-  static async deleteTask(
-    boardId: string,
-    columnId: string,
-    taskId: string
-  ): Promise<AxiosResponse> {
-    return await $api.delete(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`);
   }
 }
