52a6ceb724ac2cee64b6337419fddb9f7db64728
KononOleg <konon.oleg97@gmail.com>
2022-12-20 12:46:37 +0300

    fix: fix header

--- project-management-app/src/components/Header/index.tsx
+++ project-management-app/src/components/Header/index.tsx
@@ -1,21 +1,32 @@
 import './styles.css';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import logo from '../../assets/icon/trello-logo.svg';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from '../LanguageSwitcher';
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { authSlice } from '../../store/reducers/AuthSlice';
+import { Avatar, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip } from '@mui/material';
+import { Logout } from '@mui/icons-material';
 
 const Header: FC = () => {
   const { t } = useTranslation();
-  const { isAuth } = useAppSelector((state) => state.AuthReducer);
+  const { isAuth, user } = useAppSelector((state) => state.AuthReducer);
   const dispatch = useAppDispatch();
   const { signOut } = authSlice.actions;
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <header className="header__wrapper">
-      <Link to="/">
+      <Link to="/welcome">
         <img className="header__logo " src={logo} />
       </Link>
       <div className="header__buttons">
@@ -30,9 +41,73 @@ const Header: FC = () => {
             </Link>
           </>
         ) : (
-          <button onClick={() => dispatch(signOut())} className="header__exit">
-            {t('EXIT_BUTTON')}
-          </button>
+          <>
+            <Link to="/" className="header__boards">
+              Перейти к вашим доскам
+            </Link>
+            <>
+              <Tooltip title="Account settings">
+                <IconButton
+                  onClick={handleClick}
+                  size="small"
+                  sx={{ ml: 2 }}
+                  aria-controls={open ? 'account-menu' : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? 'true' : undefined}
+                >
+                  <Avatar sx={{ width: 32, height: 32, backgroundColor: 'red' }}>
+                    {user?.name[0]}
+                  </Avatar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 1.5,
+                    '& .MuiAvatar-root': {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    '&:before': {
+                      content: '""',
+                      display: 'block',
+                      position: 'absolute',
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: 'background.paper',
+                      transform: 'translateY(-50%) rotate(45deg)',
+                      zIndex: 0,
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+              >
+                <MenuItem>
+                  <Avatar /> Edit Profile
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={() => dispatch(signOut())}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  {t('EXIT_BUTTON')}
+                </MenuItem>
+              </Menu>
+            </>
+          </>
         )}
       </div>
     </header>
